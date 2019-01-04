ac232ea223c5d1b1efda9c81722dcc4e5a55ddf4
thelukasssheee <berner.michael@gmail.com>
2019-01-04 11:12:53 +0100

    Updated loop function

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -63,15 +63,17 @@ class DBWNode(object):
     def loop(self):
         rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
-            # TODO: Get predicted throttle, brake, and steering using `twist_controller`
-            # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            # Get predicted throttle, brake, and steering using `twist_controller`
+            if not None in (self.current_vel, self.linear_vel, self.angular_vel):
+                self.throttle, self.brake, self.steering = self.controller.control( \
+                                                                self.current_vel,
+                                                                self.dbw_enabled,
+                                                                self.linear_vel,
+                                                                self.angular_vel)
+            # Publish commands only, if DBW is enable (i.e. if not in manual mode)
+            if self.dbw_enabled:
+                self.publish(self.throttle, self.brake, self.steering)
+            # Sleep until next cycle
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
