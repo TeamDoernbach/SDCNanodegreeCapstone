03853c95fd1dc46e5ab1e6fa232ac442f586d941
Jakub Pruher <jacobnzw@gmail.com>
2019-01-04 19:21:56 +0100

    Setting a KDTree in the waypoints_cb() for quick nearest neighbor lookup. Same code as in waypoint_updater.py.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -56,6 +56,10 @@ class TLDetector(object):
 
     def waypoints_cb(self, waypoints):
         self.waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] \
+                                for waypoint in waypoints.waypoints]
+            self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
