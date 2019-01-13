65cd4942ec4f34433fd3a75ace017097c3542fbe
michael.ikemann <michael@ikemann>
2019-01-13 09:51:55 -0800

    Slight modifications of debug view

--- ros/src/doernbach_debug_view/doernbach_debug_view.py
+++ ros/src/doernbach_debug_view/doernbach_debug_view.py
@@ -20,16 +20,28 @@ class DoernbachDebugView(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/final_waypoints', Lane, self.final_waypoints_cb)
         rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
         rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
+        rospy.Subscriber('/image_color', Image, self.image_cb)
 
         self.debug_view = rospy.Publisher('doernbach_debug_view', Image, queue_size=1)
         self.start_time = time.time()
 
         self.pose = None
+
+        # waypoint informations
         self.base_waypoints = None
+        self.final_waypoints = None
         self.waypoints_2d = None
         self.waypoint_tree = None
+        self.final_waypoints_2d = None
+        self.final_waypoint_tree = None
+
+        # camera image
+        self.has_image = False
+        self.camera_image = None
+
 
         self.loop()
 
@@ -62,20 +74,27 @@ class DoernbachDebugView(object):
 
         text_size = 0.25 * scaling
 
+        car_x = 0.0
+        car_y = 0.0
+        car_dir = 0.0
+        y_off = 20 * scaling
+
         if self.pose:
-            x = self.pose.pose.position.x
-            y = self.pose.pose.position.y
-            dir = self.pose.pose.orientation.z
-            y_off = 20*scaling
+            car_x = self.pose.pose.position.x
+            car_y = self.pose.pose.position.y
+            car_dir = self.pose.pose.orientation.z
 
-            cv2.putText(image, "X: {:0.2f}".format(x), (10 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
-                        text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
-            cv2.putText(image, "Y: {:0.2f}".format(y), (60 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
-                        text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
-            cv2.putText(image, "Dir: {:0.2f}".format(dir), (110 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
-                        text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
+        if self.camera_image is not None:
 
-        cv2.line(image, (dv_width//2, dv_height//2), (int(dv_width//2+math.cos(direction)*dv_width/2), int(dv_height//2+math.sin(direction)*dv_height/2)), (255,0,0), 1)
+
+        # Print vehicle info
+        cv2.putText(image, "X: {:0.2f}".format(car_x), (10 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
+                    text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
+        cv2.putText(image, "Y: {:0.2f}".format(car_y), (60 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
+                    text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
+        cv2.putText(image, "Dir: {:0.2f}".format(car_dir), (110 * scaling, y_off), cv2.FONT_HERSHEY_SIMPLEX,
+                    text_size, (255, 0, 0), 1 * scaling, cv2.LINE_AA)
+
 
         img_msg = cv_bridge.CvBridge().cv2_to_imgmsg(image, "rgb8")
         self.debug_view.publish(img_msg)
@@ -95,6 +114,23 @@ class DoernbachDebugView(object):
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] \
                                 for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
+
+    def final_waypoints_cb(self, waypoints):
+        """
+        Get the final waypoint values from /final_waypoints
+        """
+        self.final_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.final_waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] \
+                                for waypoint in waypoints.waypoints]
+            self.final_waypoint_tree = KDTree(self.final_waypoints_2d)
+
+    def image_cb(self, msg):
+        """I
+        Receives the traffic light camera's image
+        """
+        self.has_image = True
+        self.camera_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
 
     def traffic_cb(self, msg):
